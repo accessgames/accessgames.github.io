808128252406b7863109d1869af182c3616f2a1a
accessgames <83598795+accessgames@users.noreply.github.com>
2023-11-05 11:01:15 -0800

    Update controls.js

--- controls.js
+++ controls.js
@@ -209,7 +209,7 @@ function addBtnHome() {
     e.className = 'in-game-button';
     e.id = 'inGame';
     e.innerHTML = `
-      <img src="https://accessgames.github.io/accessgameslogo.png" width="150px" height="60px" alt="Access Games">`;
+      <img src="https://accessgames.github.io/accessgameslogo.png" width="150px" height="60px" alt="Go back to ACCESS GAMES">`;
     document.getElementsByTagName('body')[0].appendChild(e);
   }
 }
