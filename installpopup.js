927f248d9580080de8580d6992afc1b4db8e6420
harveyay <83598795+harveyay@users.noreply.github.com>
2023-11-04 19:41:54 -0700

    Update installpopup.js

--- installpopup.js
+++ installpopup.js
@@ -1,34 +1,34 @@
-    if ('beforeinstallprompt' in window) {
-        // The "beforeinstallprompt" event is available in Chrome when the app can be installed.
+if ('beforeinstallprompt' in window) {
+    // The "beforeinstallprompt" event is available in Chrome when the app can be installed.
+    
+    let deferredPrompt;
 
-        let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();  // Prevent the default browser install prompt.
+        deferredPrompt = e;
 
-        window.addEventListener('beforeinstallprompt', (e) => {
-            e.preventDefault();  // Prevent the default browser install prompt.
-            deferredPrompt = e;
+        // Show the "Install App" button to the user.
+        document.getElementById('installButton').style.display = 'block';
 
-            // Show the "Install App" button to the user.
-            document.getElementById('installButton').style.display = 'block';
+        // Handle the "Install App" button click.
+        document.getElementById('installButton').addEventListener('click', () => {
+            // Show the browser's install prompt.
+            deferredPrompt.prompt();
 
-            // Handle the "Install App" button click.
-            document.getElementById('installButton').addEventListener('click', () => {
-                // Show the browser's install prompt.
-                deferredPrompt.prompt();
+            // Wait for the user's choice and handle it.
+            deferredPrompt.userChoice
+                .then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the install prompt.');
+                    } else {
+                        console.log('User dismissed the install prompt.');
+                    }
+                    // Reset the deferredPrompt to null, as it can only be used once.
+                    deferredPrompt = null;
+                });
 
-                // Wait for the user's choice and handle it.
-                deferredPrompt.userChoice
-                    .then((choiceResult) => {
-                        if (choiceResult.outcome === 'accepted') {
-                            console.log('User accepted the install prompt.');
-                        } else {
-                            console.log('User dismissed the install prompt.');
-                        }
-                        // Reset the deferredPrompt to null, as it can only be used once.
-                        deferredPrompt = null;
-                    });
-
-                // Hide the "Install App" button.
-                document.getElementById('installButton').style.display = 'none';
-            });
+            // Hide the "Install App" button.
+            document.getElementById('installButton').style.display = 'none';
         });
-    }
+    });
+}
