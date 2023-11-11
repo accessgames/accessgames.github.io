02d67639961ee39b4869b9519a63b39246a0e0e7
accessgames <83598795+accessgames@users.noreply.github.com>
2023-11-11 12:20:16 -0800

    Create gamesearch.js

---
+++ gamesearch.js
@@ -0,0 +1,11 @@
+function search_games() { 
+	let input = document.getElementById('searchbar').value 
+	input=input.toLowerCase(); 
+	let x = document.getElementsByClassName('content'); 
+	
+	for (i = 0; i < x.length; i++) { 
+		if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+			x[i].style.display="none"; 
+		} 
+	} 
+} 
